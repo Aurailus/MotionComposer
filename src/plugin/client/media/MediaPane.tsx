c0a7b35fc657a0b1c1783e181528dfd54c826f9c
Aurailus <me@auri.xyz>
2024-03-24 11:24:55 -0700

    Scene previews :)

--- src/plugin/client/media/MediaPane.tsx
+++ src/plugin/client/media/MediaPane.tsx
@@ -3,18 +3,34 @@
 import { useScenes, MotionCanvas, usePlayerState, useApplication, formatDuration} from '@motion-canvas/ui';
 import styles from './Media.module.scss';
 import { VNode } from 'preact';
-import { Scene } from '@motion-canvas/core';
+import { useRef, useLayoutEffect } from 'preact/hooks';
+import { Scene, usePlayback } from '@motion-canvas/core';
 
 function SceneItem({ scene }: { scene: Scene }) {
 	const { player } = useApplication();
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+
+	useLayoutEffect(() => {
+		function genThumbnail() {
+			scene.reset();
+			const framesToRender = player.playback.frame - scene.firstFrame;
+			const ctx = canvasRef.current!.getContext('2d');
+			(scene as any).draw(ctx);
+			for (let i = 0; i < framesToRender; i++) scene.next();
+		}
+		return scene.onCacheChanged.subscribe(genThumbnail);
+	}, [ canvasRef.current, scene ]);
 
 	return (
 		<div class={styles.media_item}>
 			<div class={styles.thumbnail}>
-				<MotionCanvas/>
-				<p>{formatDuration(player.status.framesToSeconds(scene.lastFrame - scene.firstFrame))}</p>
+				<canvas ref={canvasRef} width={1920} height={1080} class={styles.canvas}/>
 			</div>
-			<p>{scene.name}</p>
+			<div class={styles.description}>
+				<MotionCanvas/>
+				<p class={styles.name}>{scene.name}</p>
+				<p class={styles.duration}>{formatDuration(player.status.framesToSeconds(scene.lastFrame - scene.firstFrame))}</p>
+			</div>
 		</div>
 	)
 }
